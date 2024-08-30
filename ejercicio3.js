e73212429222ae27ee4730f38dbd9c136c381589
Agustina <agustinacorreo@hotmail.com>
2024-08-30 15:51:50 +0200

    ok

---
+++ ejercicio3.js
@@ -0,0 +1,48 @@
+const users = [
+  {
+    name: 'Alberto',
+    favoritesSounds: {
+      waves: { format: 'mp3', volume: 50 },
+      rain: { format: 'ogg', volume: 60 },
+      firecamp: { format: 'mp3', volume: 80 }
+    }
+  },
+  {
+    name: 'Antonio',
+    favoritesSounds: {
+      waves: { format: 'mp3', volume: 30 },
+      shower: { format: 'ogg', volume: 55 },
+      train: { format: 'mp3', volume: 60 }
+    }
+  },
+  {
+    name: 'Santiago',
+    favoritesSounds: {
+      shower: { format: 'mp3', volume: 50 },
+      train: { format: 'ogg', volume: 60 },
+      firecamp: { format: 'mp3', volume: 80 }
+    }
+  },
+  {
+    name: 'Laura',
+    favoritesSounds: {
+      waves: { format: 'mp3', volume: 67 },
+      wind: { format: 'ogg', volume: 35 },
+      firecamp: { format: 'mp3', volume: 60 }
+    }
+  }
+]
+
+const favoriteSongs = []
+
+for (const user of users) {
+  for (const song in user.favoritesSounds) {
+    if (favoriteSongs[song]) {
+      favoriteSongs[song] += 1
+    } else {
+      favoriteSongs[song] = 1
+    }
+  }
+}
+
+console.log(favoriteSongs)
